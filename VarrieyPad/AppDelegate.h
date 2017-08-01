51dc8ad349b6e9edec235cf2e3db4714c512b162
vikas <vikas@vikass-iMac.local>
2017-08-01 11:28:44 +0530

    Initial Commit

---
+++ VarrieyPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VarrieyPad
+//
+//  Created by vikas on 01/08/17.
+//  Copyright © 2017 vikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VarrieyPad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VarrieyPad
+//
+//  Created by vikas on 01/08/17.
+//  Copyright © 2017 vikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
